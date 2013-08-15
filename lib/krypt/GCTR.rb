aa10f73b3015c8db5ecef6d1d95126e8ec6d6a65
Mgetch <mattgetch009@yahoo.com>
2013-08-15 15:55:23 -0700

    Fixed inc and updated
    Fixed inc method and updated the requires to include a call to a string xor function.

--- lib/krypt/GCTR.rb
+++ lib/krypt/GCTR.rb
@@ -1,27 +1,33 @@
 require 'openSSL'
-plaintext = "two if by land"
+require 'pkcs5'
+plaintext = "testMess"
 #I believe this is what they're looking for with the inc function
 def inc(cb)
   c = 0
   additive = 0
   left_32 = cb.slice(12..15)
-  left_32.each_byte do |a|
-    additive +=  left_32.getbyte(a)
+  while c<=4
+    additive += left_32(c)
+    c+=1
   end
   additive %= 4
-  cb.setbyte(15,additive)
+  cb = cb.setbyte(15,additive)
 end
 
-def gctr(plaintext)
-  cipher = OpenSSL::Cipher::AES.new(128)
-  cipher.encrypt
-  key = cipher.random_key
-  cb = cipher.random_iv  
-  cb = cipher.update(cb) + cipher.final
-  if plaintext.length <= 16
-    encrpyted = xor(cb,plaintext)
-  end
-  else
-    encrypted = xor(cb,plaintext) 
-    inc(cb)
+class GCTR
+  def gctr(plaintext)
+    cipher = OpenSSL::Cipher::AES.new(128)
+    cipher.encrypt
+    key = cipher.random_key
+    cb = cipher.random_iv  
+    cb = cipher.update(cb) + cipher.final
+    if plaintext.length <= 16
+      $encrpyted = xor(cb,plaintext)
+      puts encrypted
+    else
+      $encrypted = xor(cb,plaintext) 
+      inc(cb)
+      puts encrypted
+      end
+    end
 end
