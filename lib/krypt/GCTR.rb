fb1e6dcc722f80f46ba985c233d03b17d25beb3b
Mgetch <mattgetch009@yahoo.com>
2013-08-20 15:05:39 -0700

    Update GCTR.rb

--- lib/krypt/GCTR.rb
+++ lib/krypt/GCTR.rb
@@ -2,7 +2,7 @@ require 'openSSL'
 require 'pkcs5'
 plaintext = "testMess"
 #I believe this is what they're looking for with the inc function
-def inc(cb)
+def inc(@cb)
   c = 0
   additive = 0
   left_32 = cb.slice(12..15)
@@ -11,21 +11,21 @@ def inc(cb)
     c+=1
   end
   additive %= 4
-  cb = cb.setbyte(15,additive)
+  @cb = cb.setbyte(15,additive)
 end
 
 class GCTR
   def gctr(plaintext)
     cipher = OpenSSL::Cipher::AES.new(128)
     cipher.encrypt
-    key = cipher.random_key
-    cb = cipher.random_iv  
-    cb = cipher.update(cb) + cipher.final
+    @@key = cipher.random_key
+    @cb = cipher.random_iv  
+    @cb = cipher.update(cb) + cipher.final
     if plaintext.length <= 16
-      $encrpyted = xor(cb,plaintext)
+      @@encrpyted = xor(cb,plaintext)
       puts encrypted
     else
-      $encrypted = xor(cb,plaintext) 
+      @@encrypted = xor(@cb,plaintext) 
       inc(cb)
       puts encrypted
       end
